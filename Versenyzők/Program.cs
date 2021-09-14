6697e4235d2fec94a4b55838587e11f8e625c381
Balázs Molnár <jozsiegykecske@gmail.com>
2021-09-14 11:09:36 +0200

    2.feladat megoldása

--- Versenyzők/Program.cs
+++ Versenyzők/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Versenyzők
 {
     class Program
     {
+        static List<Versenyzok> lista = new List<Versenyzok>();
         static void Main(string[] args)
         {
             MasodikFeladat();
@@ -16,6 +18,7 @@ namespace Versenyzők
             OtodikFeladat();
             HatodikFeladat();
             HetedikFeladat();
+            Console.ReadKey();
         }
 
         private static void HetedikFeladat()
@@ -45,7 +48,19 @@ namespace Versenyzők
 
         private static void MasodikFeladat()
         {
-            
+            StreamReader be = new StreamReader("pilotak.csv");
+            be.ReadLine();
+            while (!be.EndOfStream)
+            {
+                string[] atmeneti = be.ReadLine().Split(';');
+                int rajtszam=0;
+                if (!string.IsNullOrEmpty(atmeneti[3]))
+                {
+                    rajtszam = Convert.ToInt32(atmeneti[3]);
+                }
+                lista.Add(new Versenyzok(atmeneti[0],atmeneti[1],atmeneti[2], rajtszam));
+            }
+            be.Close();
         }
     }
 }
